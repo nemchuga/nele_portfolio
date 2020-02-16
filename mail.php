e13a5e41b428baebeb8530469eaf5c5b1dbd170e
Nemanja Ilic <35099577+nemchuga@users.noreply.github.com>
2020-02-16 21:47:49 +0100

    Initial commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $mailFrom = $_POST['mail'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $mailTo = "";
+  $headers = "Od: ".$mailFrom;
+  $txt = "Primili ste mejl od ".$name.".\n".$message;
+
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: contact.html?mailsend");
+
+}
+?>
