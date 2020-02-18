53d7e8a40238e0388e873a0d3985d2ae095ac3e6
Nemanja Ilic <35099577+nemchuga@users.noreply.github.com>
2020-02-18 19:25:06 +0100

    update to mail.php and contact.html

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 if(isset($_POST['submit'])){
   $name = $_POST['name'];
-  $mailFrom = $_POST['mail'];
+  $mailFrom = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
   $mailTo = "admin@nemanjailic.rs";
   $headers = "Od: ".$mailFrom;
-  $txt = "Primili ste mejl od ".$name.".\n".$message;
+  $txt = "Primili ste mejl od ".$name.".\n\n".$message;
 
 
   mail($mailTo, $subject, $txt, $headers);
